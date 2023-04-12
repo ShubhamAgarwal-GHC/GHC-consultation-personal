584f7bdaba96142721e95e0e70def1a3cbcf1328
ShubhamAgarwal-GHC <shubham.agarwal@ghc.health>
2023-04-12 10:47:48 +0530

    HAIR payload added

--- src/Constants/Consultation.js
+++ src/Constants/Consultation.js
@@ -48,6 +48,220 @@ const OPTIONS_COMPONENT = {
 const QUESTIONS_WITH_OPTIONS = {
   MARS: {
     SHORT: {
+      HAIR: {
+        isProgressBarRequired: false,
+        questions: [
+          {
+            questionId: 0,
+            nextQuestionid: 1,
+            previousQuestionId: -1, // req -> -1 if its the first question
+            optionType: "imageTextHorizontal",
+            type: "radio", // radio, checkbox, input, textarea (will remove)
+            question: "What best describes the current condition of your hair?", //req
+            quotes:
+              "Hair loss affects 60.5 % of the total Men population in India", //req
+            quotesDelay: 300, //req
+            defaultOptionIndex: 0, // -1 if no deafult req
+            multipleSelectAllowed: false,
+            options: [
+              {
+                displayText: "Receding hairline",
+                value: "receding_hairline",
+                image: "",
+              },
+              {
+                displayText: "Thinning at the crown",
+                value: "thinning_at_the_crown",
+                image: "",
+              },
+              {
+                displayText: "Overall hair loss/thinning",
+                value: "Overall_hair_loss/thinning",
+                image: "",
+              },
+              {
+                displayText: "receding + overall thinning",
+                value: "receding_overall_thinning",
+                image: "",
+              },
+            ],
+          },
+
+          {
+            questionId: 1,
+            nextQuestionid: 2,
+            previousQuestionId: 0, // req -> -1 if its the first question
+            optionType: "imageTextHorizontal",
+            type: "radio", // radio, checkbox, input, textarea (will remove)
+            question: "Have you tried hair loss treatments before?", //req
+            quotes: "", //req
+            quotesDelay: "", //req
+            defaultOptionIndex: 0, // -1 if no deafult req
+            multipleSelectAllowed: false,
+            options: [
+              {
+                displayText: "Yes",
+                value: "yes",
+                image: "",
+              },
+              {
+                displayText: "No",
+                value: "no",
+                image: "",
+              },
+            ],
+          },
+
+          {
+            questionId: 2,
+            nextQuestionid: 3,
+            previousQuestionId: 1, // req -> -1 if its the first question
+            optionType: "imageTextHorizontal",
+            type: "radio", // radio, checkbox, input, textarea (will remove)
+            question: "Which treatments have you tried?", //req
+            quotes: "", //req
+            quotesDelay: "", //req
+            defaultOptionIndex: 0, // -1 if no deafult req
+            multipleSelectAllowed: true,
+            options: [
+              {
+                displayText: "Oral prescription",
+                value: "oral_prescription",
+                image: "",
+              },
+              {
+                displayText: "Minoxidil",
+                value: "minoxidil",
+                image: "",
+              },
+              {
+                displayText: "Hair loss shampoo",
+                value: "hair_loss_shampoo",
+                image: "",
+              },
+              {
+                displayText: "Supplements",
+                value: "supplements",
+                image: "",
+              },
+              {
+                displayText: "Others",
+                value: "others",
+                image: "",
+              },
+            ],
+          },
+
+          {
+            questionId: 3,
+            nextQuestionid: 4,
+            previousQuestionId: 2, // req -> -1 if its the first question
+            optionType: "imageTextHorizontal",
+            type: "radio", // radio, checkbox, input, textarea (will remove)
+            question: "How did these treatments go?", //req
+            quotes: "", //req
+            quotesDelay: "", //req
+            defaultOptionIndex: 0, // -1 if no deafult req
+            multipleSelectAllowed: false,
+            options: [
+              {
+                displayText: "I saw some results ",
+                value: "i_saw_some_results ",
+                image: "",
+              },
+              {
+                displayText: "I didn't see results",
+                value: "i_didn't_see_results",
+                image: "",
+              },
+              {
+                displayText: "I didn't react well to it.",
+                value: "i_ didn't_react_well_to_it.",
+                image: "",
+              },
+            ],
+          },
+
+          {
+            questionId: 4,
+            nextQuestionid: 5,
+            previousQuestionId: 3, // req -> -1 if its the first question
+            optionType: "imageTextHorizontal",
+            type: "radio", // radio, checkbox, input, textarea (will remove)
+            question: "What results are you looking for?", //req
+            quotes:
+              "50% of the male pattern baldness can be attributed due to  hereditary roots.However, hereditary male pattern baldness is treatable by taking early preventive actions", //req
+            quotesDelay: 300, //req
+            defaultOptionIndex: 0, // -1 if no deafult req
+            multipleSelectAllowed: false,
+            options: [
+              {
+                displayText: "Regrowing your hair",
+                value: "regrowing_your_hair ",
+                image: "",
+              },
+              {
+                displayText: "Preventing future hair loss",
+                value: "preventing_future_hair_loss",
+                image: "",
+              },
+              {
+                displayText: "Both regrowth & loss prevention",
+                value: "Both regrowth & loss prevention",
+                image: "",
+              },
+            ],
+          },
+
+          {
+            questionId: 5,
+            nextQuestionid: 6,
+            previousQuestionId: 4, // req -> -1 if its the first question
+            optionType: "imageTextHorizontal",
+            type: "radio", // radio, checkbox, input, textarea (will remove)
+            question: "Does anyone in your family have hair loss?", //req
+            quotes:
+              "FACT: Every time we try a new medication it takes time for our body to react. Similarly, we can observe minimal hair loss when we start to use minoxidil for a few days", //req
+            quotesDelay: 300, //req
+            defaultOptionIndex: 0, // -1 if no deafult req
+            multipleSelectAllowed: false,
+            options: [
+              {
+                displayText: "Yes",
+                value: "yes ",
+                image: "",
+              },
+              {
+                displayText: "No",
+                value: "no",
+                image: "",
+              },
+            ],
+          },
+
+          {
+            questionId: 6,
+            nextQuestionid: -1, // req -> no more question
+            previousQuestionId: 5, // req -> -1 if its the first question
+            optionType: "imageTextHorizontal",
+            type: "radio", // radio, checkbox, input, textarea (will remove)
+            question:
+              "Our doctors need a photo of your scalp selfie photo descriptive image for how to take photo", //req
+            quotes: "", //req
+            quotesDelay: "", //req
+            defaultOptionIndex: 0, // -1 if no deafult req
+            multipleSelectAllowed: false,
+            options: [
+              // This will be a input box
+              {
+                displayText: "Yes",
+                value: "yes ",
+                image: "",
+              },
+            ],
+          },
+        ],
+      },
       SKIN: {
         isProgressBarRequired: false,
         questions: [
@@ -120,7 +334,6 @@ const QUESTIONS_WITH_OPTIONS = {
         ],
       },
       WELLNESS: {},
-      HAIR: {},
       PERFORMANCE: {},
       BEARD: {},
     },
